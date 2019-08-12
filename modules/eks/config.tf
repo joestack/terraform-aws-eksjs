260e3a2c6ee7b2f9c6fa17f16bf9fa3ebb181672
JoeStack <joern@tel2ip.net>
2019-08-12 09:06:50 +0200

    Update config.tf

--- modules/eks/config.tf
+++ modules/eks/config.tf
@@ -36,40 +36,13 @@ users:
   user:
     exec:
       apiVersion: client.authentication.k8s.io/v1alpha1
-      command: aws-iam-authenticator
+      command: aws
       args:
-        - "token"
-        - "-i"
+        - "eks"
+        - "get-token"
+        - "--cluster-name"
         - "${var.cluster-name}"
-KUBECONFIG
-  
-  kubeconfig_aws = <<KUBECONFIG
-
-apiVersion: v1
-clusters:
-- cluster:
-    server: ${aws_eks_cluster.eks.endpoint}
-    certificate-authority-data: ${aws_eks_cluster.eks.certificate_authority.0.data}
-  name: kubernetes
-contexts:
-- context:
-    cluster: kubernetes
-    user: aws
-  name: aws
-current-context: aws
-kind: Config
-preferences: {}
-users:
-- name: aws
-  user:
-    exec:
-      apiVersion: client.authentication.k8s.io/v1alpha1
-      command: aws-iam-authenticator
-      args:
-        - "token"
-        - "-i"
-        - "${var.cluster-name}"
-KUBECONFIG  
+KUBECONFIG 
 }
 
 output "config-map-aws-auth" {
@@ -78,8 +51,4 @@ output "config-map-aws-auth" {
 
 output "kubeconfig" {
   value = "${local.kubeconfig}"
-}
-
-output "kubeconfig-aws" {
-  value = "${local.kubeconfig_aws}"
 }
